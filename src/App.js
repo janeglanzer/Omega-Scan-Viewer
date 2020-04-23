48980e895d9824e7014c8d5de786242f3f7f0e46
Dyldinski <dubois.a.dylan@gmail.com>
2020-04-22 20:00:40 -0500

    Fixed input handling + minor styling

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import SelectImage from './SelectImage.jsx';
 import ImageList from './ImageList';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.saveTimes = this.saveTimes.bind(this);
+  }
   state = {
     selectedChannel: null,
     beforeTimes: null,
-    afterTimes: null
+    afterTimes: null,
+    inputSaved: false
   }
 
   beforeTimes = "1260244844.813 1259228526.063 1260219512.188 1260944009.688 1260804098.867 1260162922.688 1260239848.063 1260262343.625 1259299563.906 1260285681.813 1260201272.188 1257449232.813 1260176895.438 1260220059.313 1261486545.563 1261067040.313 1259674229.438 1259271247.125 1259236985.813 1261493573.563 1259257956.875 1259221795.063 1261866525.375 1261477028.438 1261680207.563 1257271937.938 1259255342.063 1259233408.219 1258292832.625 1261275723.625 1257303861.625 1259232455.813 1259207087.625 1257475272.813 1258203238.047 1260810096.68 1260943484.188 1260202460.938 1261344663.125 1260205095.688 1259265016.188 1259258396.438 1259240781.938 1258143921.188 1259229559.219 1260305896.406 1259252626.563 1260278743.938 1261477495.563 1260147457.063".split(' ').sort();
@@ -28,31 +33,53 @@ export default class App extends Component {
   }
 
   handleChange = (val, event) => {
-    this.setState({[val]: event.target.value});
+    this.setState({ [val]: event.target.value });
+  }
+
+  saveTimes = () => {
+    const { selectedChannel, beforeTimes, afterTimes } = this.state;
+    if (!beforeTimes || !afterTimes) {
+      window.alert("Please provide times for both before and after.")
+      return;
+    }
+
+    const tmpBefore = beforeTimes.replace(/\r?\n|\r/g, ' ').split(' ');
+    const tmpAfter = beforeTimes.replace(/\r?\n|\r/g, ' ').split(' ');
+    this.setState({ inputSaved: true, beforeTimes: tmpBefore, afterTimes: tmpAfter });
   }
 
   displayInput = () => {
-    const { selectedChannel, beforeTimes, afterTimes } = this.state;
+    const { beforeTimes, afterTimes } = this.state;
     return (
-      <div>
-        <h1>Enter Before Times</h1>
-        <textarea onChange={(event) => this.handleChange('beforeTimes', event)} value={beforeTimes} name="beforeTimes">
-        </textarea>
-        <h1>Enter After Times</h1>
-        <textarea onChange={(event) => this.handleChange('afterTimes', event)} value={afterTimes} name="afterTimes">
-        </textarea>
+      <div className="time-inputs">
+        <div>
+          <h1>Before Times</h1>
+          <textarea
+            placeholder="Enter GPS Time Coordinates in list form"
+            onChange={(event) => this.handleChange('beforeTimes', event)} value={beforeTimes || ''} name="beforeTimes">
+          </textarea>
+        </div>
+        <div>
+          <h1>After Times</h1>
+          <textarea
+            placeholder="Enter GPS Time Coordinates in list form"
+            onChange={(event) => this.handleChange('afterTimes', event)} value={afterTimes || ''} name="afterTimes">
+          </textarea>
+        </div>
+        <button type="submit"
+          onClick={this.saveTimes}>Generate Images</button>
       </div>
     );
   }
 
   render() {
-    const { selectedChannel, beforeTimes, afterTimes } = this.state;
+    const { selectedChannel, beforeTimes, afterTimes, inputSaved } = this.state;
     return (
       <div className="App">
         <div className="Header">
           <h1>Omega Scan Viewer</h1>
         </div>
-        {afterTimes ? this.displayImages() : this.displayInput()}
+        {inputSaved ? this.displayImages() : this.displayInput()}
       </div>
     );
   }
